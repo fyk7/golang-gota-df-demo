ced3ac793e8e3a847ead453a8e26e0fc3a3ad017
fyk7 <tfscofield115@gmail.com>
2022-04-09 19:44:26 +0900

    add samples

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/go-gota/gota/dataframe"
 )
@@ -24,17 +26,53 @@ func main() {
 	df := dataframe.LoadStructs(users)
 
 	var buf bytes.Buffer
-	df.Filter(
+	err := df.Filter(
 		dataframe.F{
 			Colname:    "Age",
 			Comparator: ">=",
 			Comparando: 20,
 		},
+	).Select(
+		[]string{"Age", "Name"},
 	).WriteJSON(&buf)
-	// err := df.Select([]string{"Age", "Name"}).WriteJSON(&buf)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(buf.String())
+
+	sample1()
+}
+
+func sample1() {
+	csvStr := `
+Country,Date,Age,Amount,Id
+"United States",2012-02-01,50,112.1,01234
+"United States",2012-02-01,32,321.31,54320
+"United Kingdom",2012-02-01,17,18.2,12345
+"United States",2012-02-01,32,321.31,54320
+"United Kingdom",2012-02-01,NA,18.2,12345
+"United States",2012-02-01,32,321.31,54320
+"United States",2012-02-01,32,321.31,54320
+Spain,2012-02-01,66,555.42,00241
+`
+	df := dataframe.ReadCSV(strings.NewReader(csvStr))
+
+	var buf bytes.Buffer
+	err := df.Select(
+		// TODO "Age"にNaNが入っているがこちらをどのように処理すると良いかを考察する
+		[]string{"Country", "Date", "Amount"},
+	).GroupBy(
+		"Country", "Date",
+	).Aggregation(
+		[]dataframe.AggregationType{dataframe.Aggregation_MEAN},
+		[]string{"Amount"},
+	).Select(
+		[]string{"Country", "Amount_MEAN"},
+	).WriteCSV(&buf)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(buf.String())
 
